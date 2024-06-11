8f753413c3a3c8693554820cc0937665a5aa068f
Debasish Das <debasish2000.26@gmail.com>
2024-06-11 22:17:40 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const db = require("./db");
 require("dotenv").config();
 
 // app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
 
 // Import the Router files
 const userRoutes = require("./routes/userRoutes");
